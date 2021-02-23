c9f4a9f1cb04a4b807f608a5ee29320f02de1ff6
Omer Boysal <oboysaldev@gmail.com>
2021-02-23 18:41:25 +1100

    reconfigured sass files

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,3 @@ module.exports = function(eleventyConfig)   {
 
     eleventyConfig.addPassthroughCopy("assets")
 };
-
-// module.exports = function(eleventyConfig) {
-//     eleventyConfig.addWatchTarget("./src/scss/");
-//   };
